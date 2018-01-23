7ec97979d51eff7e7f60a0e125f5f87f0cf34fd9
zengchao <57357885@qq.com>
2018-01-23 16:44:29 +0800

    网络框架接口创建

---
+++ latte-core/src/main/java/cn/sh/changxing/latte/net/RestClient.java
@@ -0,0 +1,8 @@
+package cn.sh.changxing.latte.net;
+
+/**
+ * Created by ZengChao on 2018/1/23.
+ */
+
+public class RestClient {
+}
---
+++ latte-core/src/main/java/cn/sh/changxing/latte/net/RestService.java
@@ -0,0 +1,47 @@
+package cn.sh.changxing.latte.net;
+
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
+/**
+ * Created by ZengChao on 2018/1/23.
+ */
+
+public interface RestService {
+    @GET
+    Call<String> get(@Url String url, @QueryMap Map<String, Object> params);
+
+    @FormUrlEncoded
+    @POST
+    Call<String> post(@Url String url, @FieldMap Map<String, Object> params);
+
+    @FormUrlEncoded
+    @PUT
+    Call<String> put(@Url String url, @FieldMap Map<String, Object> params);
+
+    @DELETE
+    Call<String> delete(@Url String url, @QueryMap Map<String, Object> params);
+
+    // Streaming关键字:边下载到内存边写入文件(不加的话默认一次性下载到内存再写入文件)
+    @Streaming
+    @GET
+    Call<ResponseBody> download(@Url String url, @QueryMap Map<String, Object> params);
+
+    @Multipart
+    @POST
+    Call<String> upload(@Url String url, @Part MultipartBody.Part file);
+}
